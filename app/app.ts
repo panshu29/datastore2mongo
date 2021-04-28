869deeeababb54709d3275a784db3e64953cdb10
anshpan1 <anshpan1@in.ibm.com>
2021-04-28 17:46:57 +0530

    ok

--- app/app.ts
+++ app/app.ts
@@ -7,29 +7,30 @@ var colors = require('colors');
 const program = new Command();
 program.version("0.0.1")
       .on('--help', function () {
-            console.log('*************************************************Datastore2Mongo************************************************************************************');
-            console.log(colors.brightMagenta('Info:'));
-            console.log(colors.brightGreen('   This tool converts the data from GCP Datastore format to Mongodb JSON format and then generate a migration script'));
-            console.log(colors.brightGreen('   which needs to be executed to import the JSON dataset into mongodb.'));
+            console.log(colors.brightGreen('*************************************************')+colors.brightWhite('Datastore2Mongo')+colors.brightGreen('************************************************************************************'));
+            console.log(colors.brightYellow('Info:'));
+            console.log('   This tool converts the data from GCP Datastore format to Mongodb JSON format and then generate a migration script');
+            console.log('   which needs to be executed to import the JSON dataset into mongodb. Please follow below instuction');
             console.log('');
-            console.log(colors.yellow('    1. Enviorment variable "GOOGLE_APPLICATION_CREDENTIALS" must be set with the path to the service account.'));
-            console.log(colors.yellow('    2. If database name(Namespace) is only passed as a argument , then it will create a JSON file for each table(Kind / collection) in database.'));
-            console.log(colors.yellow('    3. If database name and table name are both passe as arguments , then it will create a JSON file for that table in the database.'));
-            console.log(colors.yellow('    4. Use mongoimport cli tool to import the data to Mongodb. Ref: https://docs.mongodb.com/database-tools/mongoimport/ '));
-            console.log('*************************************************************************************************************************************');
+            console.log('    1. Enviorment variable "GOOGLE_APPLICATION_CREDENTIALS" must be set with the path to the service account.');
+            console.log('    2. mongoimport cli tool must be in classpath to enable auto data migartion. Ref: https://docs.mongodb.com/database-tools/mongoimport/');
+            console.log('    3. If database name(Namespace) is only passed as a argument , then it will create a JSON file for each table(Kind / collection) in database.');
+            console.log('    4. If database name and table name are both passe as arguments , then it will create a JSON file for that table in the database.');
+            console.log(colors.brightGreen('*****************************************************************************************************************************************************'));
 
-            console.log(colors.brightMagenta('Examples:'));
+            console.log(colors.brightYellow('Examples:'));
             console.log('');
-            console.log(colors.yellow('   $ npm run app -- -d <namespace> -h <host> -p <port>'));
-            console.log(colors.yellow('   $ npm run app -- -d <namespace> -c <kind> -h <host> -p <port>'));
-            console.log(colors.yellow('   $ npm run app -- --help'));
-            console.log(colors.yellow('   $ mongoimport --host localhost --port 27017 --jsonArray --db <namespace> --collection <kind>  --file <file.json> '));
+            console.log('   $ npm run app -- -d <namespace> -h <host> -p <port>');
+            console.log('   $ npm run app -- -d <namespace> -c <kind> -h <host> -p <port>');
+            console.log('   $ npm run app -- --help');
+            console.log('   $ mongoimport --host localhost --port 27017 --jsonArray --db <namespace> --collection <kind>  --file <file.json> ');
             console.log('');
       })
       .option('-d, --db <db>', 'database name')
       .option('-c, --collection <collection>', 'collection name')
       .option('-h, --host <host>', 'host name')
       .option('-p, --port <port>', 'port')
+      .option('-a, --auto <auto>', '[true/false/both] migration strategy')
       .action((options) => {
             if (options.db != null && options.collection != null && options.host != null && options.port != null) {
                   api.exportACollection(options.db, options.collection,options.host,options.port);
--- app/migration.ts
+++ app/migration.ts
@@ -1,6 +1,7 @@
 import { Datastore } from "@google-cloud/datastore";
 import * as fse from 'fs-extra';
-
+const nodeCmd = require('node-cmd');
+var colors = require('colors');
 
 var scriptWriter = fse.createWriteStream('script.sh', {
   flags: 'a'
@@ -24,9 +25,11 @@ export async function exportACollection(dbname: string, collectionName: string,h
   }).then(data => {
     let filepath: string = dbname + "/" + collectionName + ".json";
     let jdata = JSON.stringify(data);
-    fse.outputFile(filepath, jdata);
-    console.log(filepath + " is written to the root folder.");
-    scriptWriter.write(' mongoimport --host '+host+'--port '+port+' --jsonArray --db '+dbname+' --collection '+collectionName+' --file '+filepath+ ' && \n' );
+    fse.outputFileSync(filepath, jdata);
+    let script:string=' mongoimport --host '+host+' --port '+port+' --jsonArray --db '+dbname+' --collection '+collectionName+' --file '+filepath;
+    nodeCmd.runSync(script);
+    console.log(colors.brightGreen(filepath)+ " is written to the root folder. and collection Name "+colors.brightGreen(collectionName)+' is created in mongodb.')
+    scriptWriter.write(script+ ' && \n' );
   })
 }
 export async function exportAllCollections(dbname: string,host:string,port:string) {
@@ -34,8 +37,8 @@ export async function exportAllCollections(dbname: string,host:string,port:strin
   const query = datastore.createQuery(dbname, '__kind__').select('__key__');
   const [entities] = await datastore.runQuery(query);
   const kinds = entities.map(entity => entity[datastore.KEY].name);
-  console.log('Kinds:');
-  console.log("Started.....");
+  console.log(colors.brightRed('Kinds:'));
+  console.log(colors.brightRed("Started....."));
   scriptWriter.write('#!/bin/bash');
   scriptWriter.write('\n');
   kinds.forEach( kind => {
