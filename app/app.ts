5e146da2ed4e2d58ba11fedac9004ab55f5940d0
anshpan1 <anshpan1@in.ibm.com>
2021-04-30 09:09:31 +0530

    ok

--- app/app.ts
+++ app/app.ts
@@ -8,36 +8,36 @@ const program = new Command();
 program.version("0.0.1")
       .on('--help', function () {
             console.log(colors.brightYellow('Info:'));
-            console.log('   This tool converts the data from GCP Datastore format to Mongodb JSON format and then data will be migrated to Mongodb automcally');
-            console.log('   if the auto flag is set y else it prepare a migration script which needs to be executed manually.Please follow below instuctions!');
-            console.log('');
-            console.log('    1. Enviorment variable "GOOGLE_APPLICATION_CREDENTIALS" must be set with the path to the service account.');
-            console.log('    2. mongoimport cli tool must be in classpath to enable auto data migration. Ref: https://docs.mongodb.com/database-tools/mongoimport/');
-            console.log('    3. If database name(Namespace) is only passed as a argument , then it will create a JSON file for each table(Kind / collection) in database.');
-            console.log('    4. If database name and collection name are both passed as arguments , then it will create a JSON file for that collection in the database.');
+            console.log('   This tool converts the data from GCP Datastore format to Mongodb format and then data will be migrated to Mongodb automcally');
+            console.log('   based on the auto flag with set as "y" else it prepare a migration script which needs to be executed manually.');
+            console.log(colors.brightRed('   This project is in beta phase with version 0.0.1. This tool should be used cautiously!'));
+            console.log(colors.brightYellow('Instuctions:'));
+            console.log('    a. Enviorment variable "GOOGLE_APPLICATION_CREDENTIALS" must be set with the path to the service account credential file.');
+            console.log('    b. Mongoimport tool must be in classpath to enable auto data migration. Ref: https://docs.mongodb.com/database-tools/mongoimport/.');
+            console.log('    c. If namespace is only provided as a argument then it will migrate all Kinds (collections) to mongodb');
+            console.log('    d. If namespace and kind are both passed as arguments then it will migrate given kind to mongodb.');
             console.log(colors.brightYellow('Examples:'));
-            console.log('');
             console.log('   $ npm run app -- -d <namespace> -h <host> -p <port>');
             console.log('   $ npm run app -- -d <namespace> -c <kind> -h <host> -p <port>');
             console.log('   $ npm run app -- --help');
             console.log('   $ mongoimport --host localhost --port 27017 --jsonArray --db <namespace> --collection <kind>  --file <file.json> ');
             console.log('');
       })
-      .option('-d, --db <db>', '[all/database name]')
-      .option('-c, --collection <collection>', '[all/collection name]')
+      .option('-d, --db <db>', 'namespace name [all/<database>]')
+      .option('-c, --collection <collection>', 'kind name[all/<collection>]')
       .option('-h, --host <host>', 'host name')
-      .option('-p, --port <port>', 'port')
+      .option('-p, --port <port>', 'port number')
       .option('-a, --auto <auto>', '[y/n] migration strategy')
-      .option('-f, --full <full>', '[y/n] migration for all namespaces')
+      .option('-i, --projectid <projectid>', 'google cloud projectId')
       .action((options) => {
-            if (options.db != null && options.collection != null && options.host != null && options.port != null && options.auto != null) {
-                  api.exportACollection(options.db, options.collection, options.host, options.port, options.auto);
-            } else if (options.db != null && options.host != null && options.port != null && options.auto != null) {
-                  api.exportDataOfNamespace(options.db, options.host, options.port, options.auto);
-            } else if( options.host != null && options.port != null && options.auto != null && options.full != null) {
-                  api.exportDataOfALLNamespaces(options.host, options.port, options.auto);
+            if (options.db != null && options.collection != null && options.host != null && options.port != null && options.auto != null && options.projectid != null) {
+                  api.exportACollection(options.db, options.collection, options.host, options.port, options.auto,options.projectid);
+            } else if (options.db != null && options.host != null && options.port != null && options.auto != null && options.collection === 'all' && options.projectid != null) {
+                  api.exportAllDataOfaNamespace(options.db, options.host, options.port, options.auto,options.projectid);
+            } else if( options.host != null && options.port != null && options.auto != null && options.db === 'all' && options.projectid != null ) {
+                  api.exportAllDataOfAllNamespaces(options.host, options.port, options.auto,options.projectid);
             }else{
-                  console.log(colors.red("The manditory args. like (<db>/<host>/<port>/<auto>) is/are missing ! Please check help ('npm run d2m -- --help') for the info/examples."));
+                  console.log(colors.yellow("The manditory args. is/are missing! Please check help ('npm run d2m -- --help') for the info/examples."));
             }
       })
 program.parse(process.argv);
--- app/migration.ts
+++ app/migration.ts
@@ -3,13 +3,25 @@ import * as fse from 'fs-extra';
 const nodeCmd = require('node-cmd');
 var colors = require('colors');
 
-var scriptWriter = fse.createWriteStream('migration-script.sh', {
-  flags: 'a'
-})
+fse.mkdir('migration', { recursive: true }, (err) => {
+  if (err) console.log(err);
+});
 
-export async function exportACollection(dbname: string, collectionName: string, host: string, port: string, auto: string) {
+export async function exportACollection(dbname: string, collectionName: string, host: string, port: string, auto: string, projectId: string) {
+  let scriptWriter;
+  let flag:boolean=true;
+  if (auto === 'n' && flag===true) {
+     scriptWriter = fse.createWriteStream('migration/migration-script.sh', {
+      flags: 'a'
+    })
+    scriptWriter.write('#!/bin/bash');
+    scriptWriter.write('');
+    flag=false;
+  }
   let list: any = [];
-  const datastore = new Datastore();
+  const datastore = new Datastore({
+    projectId: projectId,
+  });
   const query = datastore.createQuery(dbname, collectionName);
   return new Promise<[]>((resolve, reject) => {
     query.run((err, entities, info) => {
@@ -23,21 +35,23 @@ export async function exportACollection(dbname: string, collectionName: string, 
       resolve(list);
     });
   }).then(data => {
-    let filepath: string = dbname + "/" + collectionName + ".json";
+    let filepath: string = "migration/" + dbname + "/" + collectionName + ".json";
     let jdata = JSON.stringify(data);
     fse.outputFileSync(filepath, jdata);
     let script: string = ' mongoimport --host ' + host + ' --port ' + port + ' --jsonArray --db ' + dbname + ' --collection ' + collectionName + ' --file ' + filepath;
     if (auto === 'y') {
       nodeCmd.runSync(script);
-      console.log(colors.brightGreen("Migration is completed for the Kind: ") + colors.brightYellow(collectionName));
+      console.log(colors.brightGreen("Migration is completed for the kind: ") + colors.brightYellow(collectionName));
     } else if (auto === 'n') {
       scriptWriter.write(script + ' && \n');
-      console.log(colors.brightGreen(" a script is writen for the Kind: ") + colors.brightYellow(collectionName));
+      console.log(colors.brightGreen("Script is written for the kind: ") + colors.brightYellow(collectionName));
     }
   })
 }
-export async function exportDataOfNamespace(dbname: string, host: string, port: string, auto: string) {
-  const datastore = new Datastore();
+export async function exportAllDataOfaNamespace(dbname: string, host: string, port: string, auto: string, projectId: string) {
+  const datastore = new Datastore({
+    projectId: projectId,
+  });
   const query = datastore.createQuery(dbname, '__kind__').select('__key__');
   const [entities] = await datastore.runQuery(query);
   const kinds = entities.map(entity => entity[datastore.KEY].name);
@@ -46,29 +60,30 @@ export async function exportDataOfNamespace(dbname: string, host: string, port: 
   }
   else {
     console.log(colors.yellow("Auto migration is off ! Use the generated script file 'migration-script.sh' to migrate manually."));
-    scriptWriter.write('#!/bin/bash');
-    scriptWriter.write('');
   }
   console.log(colors.yellow("Namespace: ") + colors.brightYellow(dbname));
-  console.log(colors.brightRed("Started....."));
+  console.log(colors.brightRed("started....."));
   kinds.forEach(kind => {
     if (!kind.startsWith("__")) {
-      exportACollection(dbname, kind, host, port, auto);
+      exportACollection(dbname, kind, host, port, auto, projectId);
     }
   });
 }
 
-export async function exportDataOfALLNamespaces(host: string, port: string, auto: string) {
-  const datastore = new Datastore();
+export async function exportAllDataOfAllNamespaces(host: string, port: string, auto: string, projectId: string) {
+  console.log(colors.brightRed("Migration of all namespaces started for projectId '" + projectId + "'"));
+  const datastore = new Datastore({
+    projectId: projectId,
+  });
+
   const query = datastore.createQuery('__namespace__').select('__key__');
   const [entities] = await datastore.runQuery(query);
   const namespaces = entities.map(entity => entity[datastore.KEY].name);
   namespaces.forEach(namespace => {
-   if(typeof(namespace)!='undefined'){ 
-    exportDataOfNamespace(namespace,host,port,auto);
-   }
+    if (typeof (namespace) != 'undefined') {
+      exportAllDataOfaNamespace(namespace, host, port, auto, projectId);
+    }
   });
-
 }
 
 
